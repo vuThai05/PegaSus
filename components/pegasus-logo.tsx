2e8f9f7e2cfecb4dfc12f89d24a146521608f176
vuThai05 <qtvu2k5@gmail.com>
2026-01-16 00:50:24 +0700

    ...

--- components/pegasus-logo.tsx
+++ components/pegasus-logo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image"
-
 interface PegasusLogoProps {
   className?: string
   size?: number
@@ -7,12 +5,29 @@ interface PegasusLogoProps {
 
 export function PegasusLogo({ className, size = 32 }: PegasusLogoProps) {
   return (
-    <Image
-      src="/file.svg"
-      alt="Pegasus Logo"
+    <svg
       width={size}
       height={size}
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="https://www.w3.org/2000/svg"
       className={className}
-    />
+    >
+      <path
+        d="M16 2L4 8v16l12 6 12-6V8L16 2z"
+        fill="currentColor"
+        className="text-primary"
+      />
+      <path
+        d="M16 6l-8 4v8l8 4 8-4v-8l-8-4z"
+        fill="currentColor"
+        className="text-background"
+      />
+      <path
+        d="M16 10l-4 2v4l4 2 4-2v-4l-4-2z"
+        fill="currentColor"
+        className="text-primary"
+      />
+    </svg>
   )
 }
